ad3bd05b74c9f7804671dd4d2b336ccee38e4834
Zavim <Zark471@gmail.com>
2023-04-21 13:30:02 -0400

    -f finish pwa setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,47 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    AstroPWA({
+      includeAssets: ["favicon.svg", "apple-touch-icon.png", "mask-icon.svg"],
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{ts,tsx,ico,png,svg}"],
+      },
+      strategies: "generateSW",
+      manifest: {
+        name: "Pixel Scene",
+        short_name: "PixelScene",
+        description: "A starter, pixel-rendered scene",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
